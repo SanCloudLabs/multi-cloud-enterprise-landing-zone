cb473aa0032b3886349b18a8a3de521fd4fd2a6d
Shan <110805599+OrangeGit1@users.noreply.github.com>
2026-06-08 14:00:24 +0530

    Add Azure network resources and peering configuration

---
+++ terraform/modules/azure_network/main.tf
@@ -0,0 +1,27 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_virtual_network" "hub" {
+  name                = "vnet-hub-shared-prod"
+  address_space       = [var.hub_cidr]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_virtual_network" "spoke" {
+  name                = "vnet-spoke-app-prod"
+  address_space       = [var.spoke_cidr]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_virtual_network_peering" "hub_to_spoke" {
+  name                         = "peer-hub-to-spoke"
+  resource_group_name          = azurerm_resource_group.rg.name
+  virtual_network_name         = azurerm_virtual_network.hub.name
+  remote_virtual_network_id    = azurerm_virtual_network.spoke.id
+  allow_virtual_network_access = true
+}
